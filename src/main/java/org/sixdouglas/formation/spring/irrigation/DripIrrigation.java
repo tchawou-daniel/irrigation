910160546619aade1e65795453605bd7c368e965
tchawou-daniel <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-03-27 14:18:50 +0100

    end step2

--- src/main/java/org/sixdouglas/formation/spring/irrigation/DripIrrigation.java
+++ src/main/java/org/sixdouglas/formation/spring/irrigation/DripIrrigation.java
@@ -6,17 +6,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @Component
 public class DripIrrigation {
     private static Logger LOGGER = LoggerFactory.getLogger(DripIrrigation.class);
 
     public Flux<Drop> followDrops() {
         //TODO Create a Flux that would emit a Drop every 20 millis seconds
-        return null;
+        return Flux.interval(Duration.ofMillis(20))
+                .map(d-> Drop.builder().greenHouseId(1)
+                        .rowId(1).dropperId(1)
+                        .instant(Instant.now())
+                        .build());
     }
 
     public Flux<Drop> followDropper(int greenHouseId, int rowId, int dropperId) {
         //TODO use the GreenHouseProducer.getDrops() function as producer, but filter the output to fit the given criteria
-        return null;
+        Flux<Drop> dropFlux = Flux.interval(Duration.ofMillis(20)).map(d->Drop.builder()
+                .greenHouseId(greenHouseId).rowId(rowId).instant(Instant.now()).dropperId(dropperId).build());
+        return dropFlux ;
     }
 }
+
+
+
+
